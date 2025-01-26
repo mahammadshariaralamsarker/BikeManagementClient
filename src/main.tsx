2a8409fc07ed687464a23d19168f639a82cbd403
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-26 13:06:38 +0600

    sdf

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import React  from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css' 
-import { Provider } from 'react-redux'
-import { store } from './redux/store.ts'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/routes.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routes.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
-</React.StrictMode>
-)
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);

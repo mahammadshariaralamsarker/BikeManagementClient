8d5181616faaf06c8eb248ca19e530a6437ef9fc
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-02-05 19:37:45 +0600

    baseapi update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://bike-ashy-omega.vercel.app/api/v1",
     credentials: "include",
     prepareHeaders: (headers) => {
       // Get Token From Redux State
